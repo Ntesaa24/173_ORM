e681c376b8167912b82d53eecb903547f3dfd879
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-10-28 15:01:30 +0700

    menambahkan post di index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = process.env.PORT || 3000; 
+
+appp.use(express.json());
+app.use(express.urlencoded({extended: false }));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () =>{
+        console.log('Server Started');
+    })
+})
+    .catch((err) => {
+        console.log(err);
+ })
+
+ app.post('/komik', async (req, res) =>{
+    const data = req.body;
+    try{
+        const komik = await db.Komik.create(data);
+        res.send(Komik);
+    } catch (error){
+        res.status(500).send({message: error.message});
+    }
+ });
+
+ 
